21d8423c1ad0737e00e6b98ea4042af68ee50926
Hugobster07 <dwf.hugoperez@gmail.com>
2026-01-21 18:01:01 -0600

    Mejora para consumir servicios

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const cors = require('cors');
 const app = express();
 const errorHandler = require('./middlewares/error.middleware');
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/api/articulos', require('./routes/articulo.routes'));
 app.use('/api/auth', require('./routes/auth.routes'));
-
-app.use(cors({
-    origin: '*', // luego lo limitamos
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 app.use(errorHandler);
 
