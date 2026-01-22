81c1c6937a5458a24c5ac39ab80c80ce149bec43
Hugobster07 <dwf.hugoperez@gmail.com>
2026-01-22 01:05:54 -0600

    Actualizacion del modelo para reportes

--- models/reporte.model.js
+++ models/reporte.model.js
@@ -4,19 +4,16 @@ const crear = async (data) => {
   try {
     const pool = await getConnection();
     await pool.request()
-  .input('Tipo', sql.VarChar, data.tipo)
-  .input('NombreArchivo', sql.VarChar, data.nombreArchivo)
-  .input('RutaArchivo', sql.VarChar, data.rutaArchivo)
-  .input('TotalRegistros', sql.Int, data.totalRegistros)
-  .input('GeneradoPor', sql.VarChar, data.generadoPor)
-  .query(`
-    INSERT INTO dbo.ReporteArticulo
-    (Tipo, NombreArchivo, RutaArchivo, TotalRegistros, GeneradoPor)
-    VALUES (@Tipo, @NombreArchivo, @RutaArchivo, @TotalRegistros, @GeneradoPor)
-  `);
-
-const result = await pool.request()
-    .query('SELECT * FROM dbo.ReporteArticulo ORDER BY FechaGeneracion DESC');
+      .input('Tipo', sql.VarChar, data.tipo)
+      .input('NombreArchivo', sql.VarChar, data.nombreArchivo)
+      .input('RutaArchivo', sql.VarChar, data.rutaArchivo)
+      .input('TotalRegistros', sql.Int, data.totalRegistros)
+      .input('GeneradoPor', sql.VarChar, data.generadoPor)
+      .query(`
+        INSERT INTO dbo.ReporteArticulo
+        (Tipo, NombreArchivo, RutaArchivo, TotalRegistros, GeneradoPor)
+        VALUES (@Tipo, @NombreArchivo, @RutaArchivo, @TotalRegistros, @GeneradoPor)
+      `);
 
   } catch (error) {
     console.error('Error al guardar reporte:', error);
@@ -24,12 +21,16 @@ const result = await pool.request()
   }
 };
 
-
 const listar = async () => {
+  try {
     const pool = await getConnection();
     const result = await pool.request()
-        .query('SELECT * FROM ReporteArticulo ORDER BY FechaGeneracion DESC');
+      .query('SELECT * FROM dbo.ReporteArticulo ORDER BY FechaGeneracion DESC');
     return result.recordset;
+  } catch (error) {
+    console.error('Error al listar reportes:', error);
+    throw error;
+  }
 };
 
 module.exports = { crear, listar };
